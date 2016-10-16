11c350643bbf790bf3c8aa45c9f4ac235bba88f6
Pierre Radermecker <pradermecker@cirb.brussels>
2016-10-16 11:18:12 +0200

    Use offline README for F1 binding

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -27,7 +27,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
 
     -- launch README
-    , ((0, xK_F1 ), spawn "chromium https://github.com/CIRB/devbox/blob/master/README.md#devbox")
+    , ((0, xK_F1 ), spawn "chromium file:///home/vagrant/.local/share/doc/devbox.html")
 
     -- launch dmenu
     , ((modm, xK_p), spawnSelected defaultGSConfig [ "chromium"  ])
